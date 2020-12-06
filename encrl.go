9e11ab6cd339399936ce53ccce02421094a10fa2
pablocorbcon <pablocorbcon@gmail.com>
2020-12-07 00:30:31 +0100

    feat: icon created at the start of the app

--- encrl.go
+++ encrl.go
@@ -6,8 +6,11 @@ import (
 )
 
 const (
-  errorColor = "\033[1;31m%s\033[0m"
-  debugColor = "\033[0;36m%s\033[0m"
+  infoColor    = "\033[1;34m%s\033[0m"
+  noticeColor  = "\033[1;36m%s\033[0m"
+  alertColor = "\033[1;33m%s\033[0m"
+  errorColor   = "\033[1;31m%s\033[0m"
+  debugColor   = "\033[0;36m%s\033[0m"
 )
 
 func parseArguments()[]string {
@@ -19,14 +22,24 @@ func parseArguments()[]string {
   args := os.Args[1:]
   if len(args) >= 2 {
     // There actually were two paths, so we should inform the user
-    fmt.Printf(debugColor, "[READING FROM]: ") 
+    fmt.Printf(noticeColor, "\n[READING FROM]:    ") 
     fmt.Println(args[0])
-    fmt.Printf(debugColor, "[WRITTING TO]: ")
+    fmt.Printf(noticeColor, "[WRITTING TO]:     ")
     fmt.Println(args[1])
   } else {
-    fmt.Printf(errorColor, "[FATAL ERROR]: Requiered 2 positional parameters - base file & output file\n")
+    fmt.Printf(errorColor, "\n[FATAL ERROR]:     Requiered 2 positional parameters - base file & output file\n")
   }
   return args
 }
 
-func main() {}
+func main() {
+  encrlIcon := `
+ ___  _  _  __  ___ _
+| __|| \| |/ _|| o \ |
+| _| | \\ ( (_ |   / |_
+|___||_|\_|\__||_|\\___|
+`
+  fmt.Printf(noticeColor, encrlIcon)
+  args := parseArguments()
+  fmt.Println(args)
+}
