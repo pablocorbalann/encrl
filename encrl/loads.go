98fe3fc015d7b31b42e0e95f12b9016416cbfff6
pablocorbcon <pablocorbcon@gmail.com>
2020-12-10 10:14:06 +0100

    feat/ci: encrl can load a json cipher using the loads module

--- encrl/loads.go
+++ encrl/loads.go
@@ -8,22 +8,77 @@
 package main 
 
 import (
-  "os"
+  "encoding/json"
   "fmt"
+  "io/ioutil"
+  "os"
+  "path/filepath"
 )
-func loadCipher (cipher string) {
+
+type JsonCipherData struct {
+  CharacterA string `json:"a"` 
+  CharacterB string `json:"b"` 
+  CharacterC string `json:"c"` 
+  CharacterD string `json:"d"` 
+  CharacterE string `json:"e"` 
+  CharacterF string `json:"f"` 
+  CharacterG string `json:"g"` 
+  CharacterH string `json:"h"` 
+  CharacterI string `json:"i"` 
+  CharacterJ string `json:"j"` 
+  CharacterK string `json:"k"` 
+  CharacterL string `json:"l"` 
+  CharacterM string `json:"m"` 
+  CharacterN string `json:"n"` 
+  CharacterO string `json:"o"` 
+  CharacterP string `json:"p"` 
+  CharacterQ string `json:"q"` 
+  CharacterR string `json:"r"` 
+  CharacterS string `json:"s"` 
+  CharacterT string `json:"t"` 
+  CharacterU string `json:"u"` 
+  CharacterV string `json:"v"` 
+  CharacterW string `json:"w"` 
+  CharacterX string `json:"x"` 
+  CharacterY string `json:"y"` 
+  CharacterZ string `json:"z"` 
+  Character0 string `json:"0"` 
+  Character1 string `json:"1"` 
+  Character2 string `json:"2"` 
+  Character3 string `json:"3"` 
+  Character4 string `json:"4"` 
+  Character5 string `json:"5"` 
+  Character6 string `json:"6"` 
+  Character7 string `json:"7"` 
+  Character8 string `json:"8"` 
+  Character9 string `json:"9"` 
+}
+
+func loadCipher (cipher string) JsonCipherData {
   /*
   * This function loads a cipher from the codifications/ folder to then return it.
   * The default cipher that is loaded is the caesar cipher.
   */
-  cipherToLoad := fmt.Sprintf("../codifications/%s.json", cipher)
-  jsonCipher, err := os.Open(cipherToLoad)
+  cipherToLoad, pathErr := filepath.Abs(fmt.Sprintf("../codifications/%s.json", cipher))
+  if pathErr != nil {
+    fmt.Printf(errorColor, "[FATAL ERROR]:      Can not get the absolute path of codification\n")
+    os.Exit(1)
+  }
+  jsonCipher, err := ioutil.ReadFile(cipherToLoad)
   if err != nil {
     fmt.Printf(errorColor, "[FATAL ERROR]:      Can not load the json cipher file\n")
     os.Exit(1)
   }
   // Inform the user about the loading cipher
+  fmt.Printf("[CIPHER]:          %s\n", cipherToLoad)
+  // Parse the json file to a cipher
+  var jsonCodificationData JsonCipherData
+  err = json.Unmarshal(jsonCipher, &jsonCodificationData)
+  if err != nil {
+    fmt.Println(errorColor, "[FATAL ERROR]:      Can not Unmarshal the json cipher file\n")
+    os.Exit(1)
+  }
   fmt.Printf(noticeColor, "[CIPHER]:          ")
-  fmt.Println(cipherToLoad)
-  fmt.Sprintf("%s", jsonCipher)
+  fmt.Println("Loaded correctly")
+  return jsonCodificationData
 }
