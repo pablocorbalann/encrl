d111ef08836fc8d2a9cefcbc98a0205663a83f8c
pablocorbcon <pablocorbcon@gmail.com>
2020-12-14 11:59:46 +0100

    chore: Distribution chore loadArguments() moved to loads.go

--- encrl/loads.go
+++ encrl/loads.go
@@ -12,9 +12,51 @@ import (
   "fmt"
   "io/ioutil"
   "os"
+  "flag"
   "path/filepath"
 )
 
+func loadArguments() (string, string, string) {
+  /*
+  * This function is used to parse all the arguments from the "os" module. It
+  * also validates the arguments before using them.
+  * Then he returns a list with the arguments
+  */
+  // Use the flag package to read all the flags and ten parse them using
+  // the Parse method
+  readingFile := flag.String("r", "", "The file to read from")
+  writingFile := flag.String("w", "", "The file to write at")
+  codificationTable := flag.String("c", "caesar", "The codification the program should use.")
+  flag.Parse()
+  fmt.Printf("\n[PARSING]:         r %s | w  %s\n", *readingFile, *writingFile)
+  fmt.Printf("[CODIFICATION]:    %s\n", *codificationTable)
+  // Create basic variables to then return them and check if there•
+  // is a reading file and a writing file
+  var readingFileRoute, writingFileRoute string
+  if *readingFile == "" || *writingFile == "" {
+    // One of the flags is missing so we can't parse them
+    fmt.Printf(errorColor, "[FATAL ERROR]:     Parse error, one of the flags is missing\n")
+    os.Exit(1)
+  } 
+  // Convert from the relative path to the absolute path using the filepath module
+  readingFileRoute, readingErr := filepath.Abs(*readingFile)
+  writingFileRoute, writingErr := filepath.Abs(*writingFile)
+  if readingErr == nil && writingErr == nil {
+    // There actually were two paths, so we should inform the user
+    fmt.Printf(noticeColor, "\n[READING FROM]:    ")
+    fmt.Println(readingFileRoute)
+    fmt.Printf(noticeColor, "[WRITTING TO]:     ")
+    fmt.Println(writingFileRoute)
+    // Return the complete routes
+    return readingFileRoute, writingFileRoute, *codificationTable
+  } else {
+    fmt.Printf(errorColor, "[FATAL ERROR]:     One of the routes is not valid\n")
+    os.Exit(1)
+  } 
+  return "", "", ""
+}
+    
+ 
 func loadCipher (cipher string) map[string]string {
   /*
   * This function loads a cipher from the codifications/ folder to then return it.
--- encrl/main.go
+++ encrl/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
   "fmt"
-  "os"
-  "flag"
-  "path/filepath"
 )
 
 const (
@@ -14,46 +11,6 @@ const (
   errorColor   = "\033[1;31m%s\033[0m"
   debugColor   = "\033[0;36m%s\033[0m"
 )
-
-func parseArguments() (string, string, string) {
-  /*
-  * This function is used to parse all the arguments from the "os" module. It
-  * also validates the arguments before using them.
-  * Then he returns a list with the arguments
-  */
-  // Use the flag package to read all the flags and ten parse them using
-  // the Parse method
-  readingFile := flag.String("r", "", "The file to read from")
-  writingFile := flag.String("w", "", "The file to write at")
-  codificationTable := flag.String("c", "caesar", "The codification the program should use.")
-  flag.Parse()
-  fmt.Printf("\n[PARSING]:         r %s | w  %s\n", *readingFile, *writingFile)
-  fmt.Printf("[CODIFICATION]:    %s\n", *codificationTable)
-  // Create basic variables to then return them and check if there 
-  // is a reading file and a writing file
-  var readingFileRoute, writingFileRoute string
-  if *readingFile == "" || *writingFile == "" {
-    // One of the flags is missing so we can't parse them
-    fmt.Printf(errorColor, "[FATAL ERROR]:     Parse error, one of the flags is missing\n")
-    os.Exit(1)
-  }
-  // Convert from the relative path to the absolute path using the filepath module
-  readingFileRoute, readingErr := filepath.Abs(*readingFile)
-  writingFileRoute, writingErr := filepath.Abs(*writingFile)
-  if readingErr == nil && writingErr == nil {
-    // There actually were two paths, so we should inform the user
-    fmt.Printf(noticeColor, "\n[READING FROM]:    ")
-    fmt.Println(readingFileRoute)
-    fmt.Printf(noticeColor, "[WRITTING TO]:     ")
-    fmt.Println(writingFileRoute)
-    // Return the complete routes
-    return readingFileRoute, writingFileRoute, *codificationTable
-  } else {
-    fmt.Printf(errorColor, "[FATAL ERROR]:     One of the routes is not valid\n")
-    os.Exit(1)
-  }
-  return "", "", ""
-}
 
 func main() {
   encrlIcon := `
@@ -65,7 +22,7 @@ d88888b      d8b   db       .o88b.      d8888b.      db
 Y88888P      VP   V8P       Y88P'       88   YD      Y88888P 
 `
   fmt.Printf(noticeColor, encrlIcon)
-  readingFile, writingFile, codification := parseArguments()
+  readingFile, writingFile, codification := loadArguments()
   loadCipher(codification)
   fmt.Sprintf(readingFile, writingFile)
 }
